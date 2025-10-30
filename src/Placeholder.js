91088436d838f14213969092daef7d15a3af8c7c
Muhammad Hendra Pranajaya <34448889+kokikoding@users.noreply.github.com>
2025-10-30 12:40:09 +0700

    Update code

--- src/Placeholder.js
+++ src/Placeholder.js
@@ -1,73 +1,78 @@
 /*
-* @flow
-*/
+ * @flow
+ */
 
 import React, { Component } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
+import { PlaceholderContext } from './PlaceholderContainer'; // ✅ import shared context
 import PropTypes from 'prop-types';
-import {
-  View,
-  StyleSheet,
-  Animated,
-} from 'react-native';
 
 type PlaceholderProps = {
   style: Object,
-  animatedComponent: React.Element<*>
+  animatedComponent: React.Element<*>,
 };
 
 type PlaceholderState = {
   x: number,
   width: number,
   isMeasured: boolean,
-  resolved: boolean
+  resolved: boolean,
 };
 
-export default class Placeholder extends Component {
-  state: PlaceholderState;
-  props: PlaceholderProps;
+export default class Placeholder extends Component<
+  PlaceholderProps,
+  PlaceholderState
+> {
+  static contextType = PlaceholderContext; // ✅ use modern context
+  context: React.ContextType<typeof PlaceholderContext>;
+
   constructor(props: PlaceholderProps) {
     super(props);
     this.state = {
       x: 0,
       width: 0,
       isMeasured: false,
-      resolved: false
+      resolved: false,
     };
   }
 
   componentDidMount(): void {
-    this.context.registerPlaceholder(this);
+    const ctx = this.context;
+    if (ctx && typeof ctx.registerPlaceholder === 'function') {
+      ctx.registerPlaceholder(this);
+    }
   }
 
   render(): React.Element<*> {
     const { style, children } = this.props;
     const { x, isMeasured, resolved } = this.state;
-    const { animatedComponent, position } = this.context;
+    const { animatedComponent, position } = this.context || {};
 
     if (resolved) {
-      return children
+      return children;
     }
+
     const animatedStyle = {
       height: '100%',
       width: '100%',
-      transform: [{ translateX: position }],
-      left: -x
+      transform: [{ translateX: position || new Animated.Value(0) }],
+      left: -x,
     };
+
     return (
       <View
         ref={ref => {
           this.testRef = ref;
         }}
         onLayout={this._setDimensions}
-        style={[style, styles.overflow]}
-      >
-        {isMeasured &&
-          <Animated.View style={animatedStyle}>
-            {animatedComponent}
-          </Animated.View>}
+        style={[style, styles.overflow]}>
+        {isMeasured && animatedComponent && (
+          <Animated.View style={animatedStyle}>{animatedComponent}</Animated.View>
+        )}
       </View>
     );
   }
+
   _resolve = () => {
     this.setState(() => ({ resolved: true }));
   };
@@ -78,15 +83,8 @@ export default class Placeholder extends Component {
   };
 }
 
-Placeholder.contextTypes = {
-    position: PropTypes.object.isRequired,
-    animatedComponent: PropTypes.object.isRequired,
-    registerPlaceholder: PropTypes.func.isRequired
-}
-
-
 const styles = StyleSheet.create({
   overflow: {
-    overflow: 'hidden'
-  }
+    overflow: 'hidden',
+  },
 });
--- src/PlaceholderContainer.js
+++ src/PlaceholderContainer.js
@@ -1,19 +1,24 @@
 /*
-* @flow
-*/
+ * @flow
+ */
 
-import React, { Component } from 'react';
+import React, { Component, createContext } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Text,
   View,
   StyleSheet,
   Animated,
   Dimensions,
-  Platform
 } from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
+
+// ✅ NEW: create modern Context for placeholders
+export const PlaceholderContext = createContext({
+  position: new Animated.Value(0),
+  animatedComponent: null,
+  registerPlaceholder: () => {},
+});
 
 type PlaceholderContainerProps = {
   duration: number,
@@ -21,59 +26,58 @@ type PlaceholderContainerProps = {
   style: Object,
   animatedComponent: React.Element<*>,
   loader: Function,
-  replace: boolean
+  replace: boolean,
 };
 
 type PlaceholderContainerState = {
-  containerComponent: Object,
-  animatedComponent: Object,
+  ContainerComponent: Object,
+  AnimatedComponent: Object,
   startPosition: number,
   stopPosition: number,
-  isContainerMeasured: boolean,
+  isContainerComponentMeasured: boolean,
   isAnimatedComponentMeasured: boolean,
-  Component: React.Element<*>
+  Component: React.Element<*>,
 };
 
-export default class PlaceholderContainer extends Component {
-  props: PlaceholderContainerProps;
-  state: PlaceholderContainerState;
+export default class PlaceholderContainer extends Component<
+  PlaceholderContainerProps,
+  PlaceholderContainerState
+> {
   position: Animated.Value;
   placeholders: Array<React.Element<*>>;
+
   constructor(props: PlaceholderContainerProps) {
     super(props);
     this.state = {
-      ContainerComponent: { x: 0, y: 0, width: 0, height: 0 },
-      AnimatedComponent: { x: 0, y: 0, width: 0, height: 0 },
+      ContainerComponent: {x: 0, y: 0, width: 0, height: 0},
+      AnimatedComponent: {x: 0, y: 0, width: 0, height: 0},
       startPosition: 0,
       stopPosition: 0,
       isContainerComponentMeasured: false,
       isAnimatedComponentMeasured: false,
-      Component: null
+      Component: null,
     };
     this.position = new Animated.Value(0);
     this.placeholders = [];
+
     this._measureContainerComponent = this._measureView.bind(
       null,
-      'ContainerComponent'
+      'ContainerComponent',
     );
     this._measureAnimatedComponent = this._measureView.bind(
       null,
-      'AnimatedComponent'
+      'AnimatedComponent',
     );
   }
-  getChildContext(): Object {
-    return {
-      position: this.position,
-      animatedComponent: this.props.animatedComponent,
-      registerPlaceholder: this._registerPlaceholder
-    };
-  }
+
   componentDidMount(): void {
-    const { loader, replace } = this.props;
-    loader &&
+    const {loader, replace} = this.props;
+    if (loader) {
       Promise.resolve(loader).then(Component => {
-        !replace ? this.setState({ Component }) : this._replacePlaceholders();
+        if (!replace) this.setState({Component});
+        else this._replacePlaceholders();
       });
+    }
   }
 
   componentWillUnmount(): void {
@@ -81,71 +85,78 @@ export default class PlaceholderContainer extends Component {
   }
 
   render(): React.Element<*> {
-    const { style, elements, animatedComponent, children } = this.props;
-    const { Component, isAnimatedComponentMeasured } = this.state;
+    const {style, animatedComponent, children} = this.props;
+    const {Component, isAnimatedComponentMeasured} = this.state;
+
     return (
       <View
         ref={ref => {
           this.containerRef = ref;
         }}
         onLayout={this._measureContainerComponent}
-        style={style}
-      >
-        {!isAnimatedComponentMeasured &&
+        style={style}>
+        {!isAnimatedComponentMeasured && (
           <View
             ref={ref => {
               this.componentRef = ref;
             }}
             onLayout={this._measureAnimatedComponent}
-            style={styles.offscreen}
-          >
+            style={styles.offscreen}>
             {animatedComponent}
-          </View>}
-        {(Component && Component) || children}
+          </View>
+        )}
+
+        {/* ✅ wrap children with modern context provider */}
+        <PlaceholderContext.Provider
+          value={{
+            position: this.position,
+            animatedComponent: animatedComponent,
+            registerPlaceholder: this._registerPlaceholder,
+          }}>
+          {(Component && Component) || children}
+        </PlaceholderContext.Provider>
       </View>
     );
   }
 
   _triggerAnimation = (): void => {
-    const { duration, delay } = this.props;
-    const { startPosition, stopPosition } = this.state;
-    Animated.loop(Animated.sequence([
-      Animated.timing(this.position, {
-        toValue: stopPosition || screenWidth,
-        duration: duration,
-        useNativeDriver: true,
-        isInteraction: false
-      }),
-      Animated.timing(this.position, {
-        toValue: startPosition || 0,
-        duration: 0,
-        delay: delay || 0,
-        useNativeDriver: true,
-        isInteraction: false
-      })
-    ])).start();
+    const {duration, delay} = this.props;
+    const {startPosition, stopPosition} = this.state;
+
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(this.position, {
+          toValue: stopPosition || screenWidth,
+          duration: duration,
+          useNativeDriver: true,
+          isInteraction: false,
+        }),
+        Animated.timing(this.position, {
+          toValue: startPosition || 0,
+          duration: 0,
+          delay: delay || 0,
+          useNativeDriver: true,
+          isInteraction: false,
+        }),
+      ]),
+    ).start();
   };
 
   _startAndRepeat = (): void => {
-    const { Component } = this.state;
-    !Component && this._triggerAnimation();
+    const {Component} = this.state;
+    if (!Component) this._triggerAnimation();
   };
 
   _measureView = (viewName: string, event: Object): void => {
-    const { x, y, height, width } = event.nativeEvent.layout;
+    const {x, y, height, width} = event.nativeEvent.layout;
     this.setState(
       () => ({
-        [viewName]: {
-          x,
-          y,
-          height,
-          width
-        },
-        [`is${viewName}Measured`]: true
+        [viewName]: {x, y, height, width},
+        [`is${viewName}Measured`]: true,
       }),
       () => {
         this._setAnimationPositions();
-      }
+      },
     );
   };
 
@@ -154,54 +165,50 @@ export default class PlaceholderContainer extends Component {
       ContainerComponent,
       AnimatedComponent,
       isContainerComponentMeasured,
-      isAnimatedComponentMeasured
+      isAnimatedComponentMeasured,
     } = this.state;
-    if (!isContainerComponentMeasured || !isAnimatedComponentMeasured) {
-      return;
-    }
+    if (!isContainerComponentMeasured || !isAnimatedComponentMeasured) return;
+
     const startPosition = -(ContainerComponent.x + AnimatedComponent.width);
     const stopPosition =
-      ContainerComponent.x + ContainerComponent.width + AnimatedComponent.width;
+      ContainerComponent.x +
+      ContainerComponent.width +
+      AnimatedComponent.width;
+
     this.setState(
       () => ({
         startPosition,
-        stopPosition
+        stopPosition,
       }),
       () => {
         this.position.setValue(startPosition);
         this._startAndRepeat();
-      }
+      },
     );
   };
 
   _registerPlaceholder = (placeholder: React.Element<*>): void => {
-    const { replace } = this.props;
-    if (!replace) {
-      return;
-    }
+    const {replace} = this.props;
+    if (!replace) return;
     this.placeholders.push(placeholder);
   };
 
   _replacePlaceholders = (): void => {
     try {
       this.placeholders.forEach(placeholder => {
-        placeholder._resolve();
+        if (placeholder && placeholder._resolve) {
+          placeholder._resolve();
+        }
       });
     } catch (e) {
-      console.log('Something went wrong');
+      console.log('Something went wrong replacing placeholders:', e);
     }
   };
 }
 
-PlaceholderContainer.childContextTypes = {
-  position: PropTypes.object.isRequired,
-  animatedComponent: PropTypes.object.isRequired,
-  registerPlaceholder: PropTypes.func.isRequired
-};
-
 const styles = StyleSheet.create({
   offscreen: {
     position: 'absolute',
-    left: -1000
-  }
+    left: -1000,
+  },
 });
